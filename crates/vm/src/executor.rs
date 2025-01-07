781d4058b4a0cc2098d8011c32f6916c318c3682
πa <76558220+rkdud007@users.noreply.github.com>
2025-01-07 18:14:09 +0900

    smth like this

--- crates/vm/src/executor.rs
+++ crates/vm/src/executor.rs
@@ -5,7 +5,10 @@ use alloy_primitives::{B256, U256};
 use eyre::OptionExt;
 use ress_storage::Storage;
 use reth_chainspec::ChainSpec;
-use reth_evm::{execute::BlockExecutionStrategy, ConfigureEvm, ConfigureEvmEnv};
+use reth_evm::{
+    execute::{BlockExecutionStrategy, ExecuteOutput},
+    ConfigureEvm, ConfigureEvmEnv,
+};
 use reth_evm_ethereum::{execute::EthExecutionStrategy, EthEvmConfig};
 use reth_primitives::{BlockWithSenders, Receipt, SealedBlock};
 use reth_primitives_traits::transaction::signed::SignedTransaction;
@@ -41,66 +44,28 @@ impl BlockExecutor {
         &mut self,
         block: &SealedBlock,
     ) -> Result<BlockExecutionOutput<Receipt>, EvmError> {
-        // todo: is this correct
-        // let total_difficulty = self
-        //     .chain_config()
-        //     .final_paris_total_difficulty(block.number)
-        //     .unwrap();
-        //TODO: smth like this
-        self.strategy.apply_pre_execution_changes(
-            &BlockWithSenders::new(block.unseal(), block.senders().unwrap()).unwrap(),
-            U256::ZERO,
-        );
-        let mut receipts = Vec::new();
-        let mut cumulative_gas_used = 0;
+        //TODO: smth like this, rn mocked
+        let block =
+            BlockWithSenders::new(block.clone().unseal(), block.senders().unwrap()).unwrap();
+        let total_difficulty = U256::ZERO;
+        self.strategy
+            .apply_pre_execution_changes(&block, total_difficulty)
+            .unwrap();
+        let ExecuteOutput { receipts, gas_used } = self
+            .strategy
+            .execute_transactions(&block, total_difficulty)
+            .unwrap();
+        let requests = self
+            .strategy
+            .apply_post_execution_changes(&block, total_difficulty, &receipts)
+            .unwrap();
+        let state = self.strategy.finish();
 
-        for transaction in block.body.transactions.iter() {
-            let header = &block.header;
-            let block_env = BlockEnv {
-                number: U256::from(header.number),
-                coinbase: header.header().beneficiary,
-                timestamp: U256::from(header.timestamp),
-                gas_limit: U256::from(header.gas_limit),
-                basefee: U256::from(header.base_fee_per_gas.unwrap_or(INITIAL_BASE_FEE)),
-                difficulty: header.difficulty,
-                prevrandao: Some(header.header().mix_hash),
-                blob_excess_gas_and_price: Some(BlobExcessGasAndPrice::new(
-                    header.excess_blob_gas.unwrap_or_default(),
-                )),
-            };
-            let mut tx_env = TxEnv::default();
-            self.eth_evm_config.fill_tx_env(
-                &mut tx_env,
-                transaction,
-                transaction
-                    .recover_signer_unchecked()
-                    .ok_or_eyre("failed to recover sender")
-                    .unwrap(),
-            );
-            let db = &mut self.state.database;
-            // todo: not sure how i can construct relevant env
-            let mut cfg = CfgEnvWithHandlerCfg::new(Default::default(), Default::default());
-            self.eth_evm_config
-                .fill_cfg_env(&mut cfg, header, total_difficulty);
-            let env_hander_cfg = EnvWithHandlerCfg::new_with_cfg_env(cfg, block_env, tx_env);
-            let mut evm = self.eth_evm_config.evm_with_env(db, env_hander_cfg);
-            // todo: rn error with `RejectCallerWithCode`
-            let result = evm.transact_commit().unwrap();
-
-            cumulative_gas_used += result.gas_used();
-            let receipt = Receipt {
-                tx_type: transaction.tx_type(),
-                success: result.is_success(),
-                cumulative_gas_used,
-                logs: result.logs().to_vec(),
-            };
-            receipts.push(receipt);
-        }
-
-        if let Some(_withdrawals) = &block.body.withdrawals {
-            //todo: process withdrawl
-        }
-
-        todo!()
+        Ok(BlockExecutionOutput {
+            state,
+            receipts,
+            requests,
+            gas_used,
+        })
     }
 }
