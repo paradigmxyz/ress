599e54e5b9deff15a5ca1f5d0086d2b8829f1f47
πa <76558220+rkdud007@users.noreply.github.com>
2025-02-11 09:43:40 +0900

    time

--- crates/engine/src/downloader.rs
+++ crates/engine/src/downloader.rs
@@ -36,7 +36,7 @@ impl EngineDownloader {
         Self {
             network,
             consensus,
-            retry_delay: Duration::from_millis(40),
+            retry_delay: Duration::from_millis(60),
             inflight_full_block_requests: Vec::new(),
             inflight_witness_requests: Vec::new(),
             inflight_bytecode_requests: Vec::new(),
