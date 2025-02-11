6f215e4e643a12f6c91b4e6594193eb3ff4ae8d3
πa <76558220+rkdud007@users.noreply.github.com>
2025-02-11 09:29:43 +0900

    fix: timeout

--- crates/engine/src/downloader.rs
+++ crates/engine/src/downloader.rs
@@ -36,7 +36,7 @@ impl EngineDownloader {
         Self {
             network,
             consensus,
-            retry_delay: Duration::from_millis(50),
+            retry_delay: Duration::from_millis(40),
             inflight_full_block_requests: Vec::new(),
             inflight_witness_requests: Vec::new(),
             inflight_bytecode_requests: Vec::new(),
